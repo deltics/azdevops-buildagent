40b6e04922a6a61c53c736706ffb840f728b89b1
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-09-06 10:41:24 +1200

    feat: Added gcc11 to long-form image tag in build and push scripts

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ docker build -t deltics/azdevops-buildagent:2.170.1 2.170.1
 docker build -t deltics/azdevops-buildagent:2.190.0 2.190.0
 
 # GoLang images
-docker build -t deltics/azdevops-buildagent:2.170.1-docker20.10.8-dotnet5.0-gitversion5.6.6-go1.16.7-helm3.6.3-jdk11-jfrog2-maven3.8.2 2.170.1-wakatipu
-docker build -t deltics/azdevops-buildagent:2.190.0-docker20.10.8-dotnet5.0-gitversion5.6.6-go1.16.7-helm3.6.3-jdk11-jfrog2-maven3.8.2 2.190.0-wakatipu
+docker build -t deltics/azdevops-buildagent:2.170.1-docker20.10.8-dotnet5.0-gcc11-gitversion5.6.6-go1.16.7-helm3.6.3-jdk11-jfrog2-maven3.8.2 2.170.1-wakatipu
+docker build -t deltics/azdevops-buildagent:2.190.0-docker20.10.8-dotnet5.0-gcc11-gitversion5.6.6-go1.16.7-helm3.6.3-jdk11-jfrog2-maven3.8.2 2.190.0-wakatipu
 
 # Short/alternate tags
 docker build -t deltics/azdevops-buildagent:2.170.1-wakatipu 2.170.1-wakatipu
--- push.sh
+++ push.sh
@@ -7,8 +7,8 @@ docker push deltics/azdevops-buildagent:2.170.1
 docker push deltics/azdevops-buildagent:2.190.0
 
 # GoLang images
-docker push deltics/azdevops-buildagent:2.170.1-docker20.10.8-dotnet5.0-gitversion5.6.6-go1.16.7-helm3.6.3-jdk11-jfrog2-maven3.8.2
-docker push deltics/azdevops-buildagent:2.190.0-docker20.10.8-dotnet5.0-gitversion5.6.6-go1.16.7-helm3.6.3-jdk11-jfrog2-maven3.8.2
+docker push deltics/azdevops-buildagent:2.170.1-docker20.10.8-dotnet5.0-gcc11-gitversion5.6.6-go1.16.7-helm3.6.3-jdk11-jfrog2-maven3.8.2
+docker push deltics/azdevops-buildagent:2.190.0-docker20.10.8-dotnet5.0-gcc11-gitversion5.6.6-go1.16.7-helm3.6.3-jdk11-jfrog2-maven3.8.2
 
 # Short/alternate tags
 docker push deltics/azdevops-buildagent:2.170.1-wakatipu
