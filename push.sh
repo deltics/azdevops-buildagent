ac5216c433a542e5d72df46e3be211814367d315
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-11-11 19:01:04 +1300

    feat: added images using agent software version 2.181.2

--- push.sh
+++ push.sh
@@ -4,12 +4,17 @@ docker push deltics/azdevops-buildagent:base
 
 # Build agent base images (different versions)
 docker push deltics/azdevops-buildagent:2.170.1
+docker push deltics/azdevops-buildagent:2.181.2
 docker push deltics/azdevops-buildagent:2.190.0
 
 # GoLang images
-docker push deltics/azdevops-buildagent:2.170.1-docker20.10.8-dotnet5.0-gcc11-gitversion5.6.6-go1.16.7-helm3.6.3-jdk11-jfrog2-maven3.8.2
-docker push deltics/azdevops-buildagent:2.190.0-docker20.10.8-dotnet5.0-gcc11-gitversion5.6.6-go1.16.7-helm3.6.3-jdk11-jfrog2-maven3.8.2
+contentTag="docker20.10.8-dotnet5.0-gcc11-gitversion5.6.6-go1.16.7-helm3.6.3-jdk11-jfrog2-maven3.8.3"
+
+docker push deltics/azdevops-buildagent:2.170.1-docker20.10.8-$contentTag
+docker push deltics/azdevops-buildagent:2.181.2-docker20.10.8-$contentTag
+docker push deltics/azdevops-buildagent:2.190.0-docker20.10.8-$contentTag
 
 # Short/alternate tags
 docker push deltics/azdevops-buildagent:2.170.1-wakatipu
+docker push deltics/azdevops-buildagent:2.181.2-wakatipu
 docker push deltics/azdevops-buildagent:2.190.0-wakatipu
