e088a588c8f1ef41029b25955b4c0e235100c014
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2023-05-25 12:34:22 +1200

    [fix] replace 'scan' with 'scout cves'

--- .build/scan.sh
+++ .build/scan.sh
@@ -3,6 +3,6 @@
 # Scan only the most fully equipped images - any vulnerabilities in base layers
 #  will be identified as such, if present.
 
-docker scan deltics/azdevops-buildagent:2.170.1-wakatipu
-docker scan deltics/azdevops-buildagent:2.181.2-wakatipu
-docker scan deltics/azdevops-buildagent:2.190.0-wakatipu
+docker scout cves deltics/azdevops-buildagent:2.170.1-wakatipu
+docker scout cves deltics/azdevops-buildagent:2.181.2-wakatipu
+docker scout cves deltics/azdevops-buildagent:2.190.0-wakatipu
